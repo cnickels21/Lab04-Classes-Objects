15a02897f8cddbc6a096600a9e24f2315c8b4a71
cnickels21 <cnickels_21@yahoo.com>
2020-05-07 16:34:17 -0500

    first test failed!

--- Lab04_TicTacToeTests/Lab04_TicTacToeTests.cs
+++ Lab04_TicTacToeTests/Lab04_TicTacToeTests.cs
@@ -1,3 +1,4 @@
+using Lab04_TicTacToe.Classes;
 using System;
 using Xunit;
 
@@ -6,9 +7,26 @@ namespace Lab04_TicTacToeTests
     public class Lab04_TicTacToeTests
     {
         [Fact]
-        public void Test1()
+        public void Check_that_turn_switches_after_each_player_plays()
         {
+            // Arrange
+            Player player1 = new Player();
+            player1.Name = "Chase";
 
+            Player player2 = new Player();
+            player2.Name = "Marie";
+
+            player1.IsTurn = true;
+            player2.IsTurn = false;
+
+            
+
+            // Act
+            Game testGameBoard = new Game(player1, player2);
+            Player current = testGameBoard.NextPlayer();
+
+            // Assert
+            Assert.Equal("Marie", current.Name);
         }
     }
 }
