f4d493175ece7e041946e6b3ca92af9a090acfb9
cnickels21 <cnickels_21@yahoo.com>
2020-05-07 16:11:42 -0500

    all class files made public

---
+++ Lab04_TicTacToeTests/Lab04_TicTacToeTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Lab04_TicTacToeTests
+{
+    public class Lab04_TicTacToeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
